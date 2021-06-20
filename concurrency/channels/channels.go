14dbbd380aa736fee21985841bccf09f471aa367
LuisEduNasc <luisedu_nascimento@hotmail.com>
2021-06-20 18:30:45 -0300

    feat(worker): added concurrency examples

---
+++ concurrency/channels/channels.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	canal := make(chan string)
+
+	go escrever("Olá mundo!", canal)
+
+	fmt.Println("Depois da função escrever começar a ser executada")
+
+	for mensagem := range canal {
+		fmt.Println(mensagem)
+	}
+
+	fmt.Println("Fim do programa!")
+}
+
+func escrever(text string, canal chan string) {
+	for i := 0; i < 5; i++ {
+		canal <- text
+		time.Sleep(time.Second)
+	}
+
+	close(canal)
+}
